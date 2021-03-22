bec4c602105d9c192cb8a1d5cb22460aa1e1a0e7
Hikmat Samadov <hikmetsemedov0177@gmail.com>
2021-03-22 14:23:52 +0400

    Part 11 - Success | Search and Sitemaps

--- saulgadgets/urls.py
+++ saulgadgets/urls.py
@@ -2,23 +2,31 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.sitemaps.views import sitemap
 
 from apps.cart.webhook import webhook
 from apps.core.views import frontpage, contact, about
-from apps.store.views import  product_detail, category_detail
+from apps.store.views import  product_detail, category_detail, search
 from apps.cart.views import cart_detail, success
 
 from apps.coupon.api import api_can_use
 from apps.store.api import api_add_to_cart, api_remove_from_cart, api_checkout, create_checkout_session
 
+from .sitemaps import StaticViewSitemap, CategorySitemap, ProductSitemap
+
+sitemaps = {'static': StaticViewSitemap, 'product': ProductSitemap, 'category': CategorySitemap}
+
 urlpatterns = [
     path('', frontpage, name='frontpage'),
+    path('search/', search, name='search'),
     path('contact/', contact, name='contact'),
     path('about/', about, name='about'),
     path('cart/', cart_detail, name='cart'),
     path('hooks/', webhook, name='webhook'),
     path('cart/success/', success, name='success'),
     path('admin/', admin.site.urls),
+    # Sitemap
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
     # API
     path('api/can_use/', api_can_use, name='api_can_use'),
     path('api/create_checkout_session/', create_checkout_session, name='create_checkout_session'),
