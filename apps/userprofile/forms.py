42671fe8cf91a77377ec687766a31ea9c1c54da4
Hikmat Samadov <hikmetsemedov0177@gmail.com>
2021-03-24 19:09:27 +0400

    Part 17 - Success || Forms are prettier now

--- apps/userprofile/forms.py
+++ apps/userprofile/forms.py
@@ -7,6 +7,13 @@ class SignUpForm(UserCreationForm):
     last_name = forms.CharField(max_length=50, required=True)
     email = forms.EmailField(max_length=255, required=True)
 
+    def __init__(self, *args, **kwargs):
+        super(SignUpForm, self).__init__(*args, **kwargs)
+        #TODO Learn this type of writing (above)
+        # Adding 'input' class to form fields created by django (below)
+        for field_name in self.fields:
+            self.fields[field_name].widget.attrs['class'] = 'input'
+
     class Meta:
         model = User
         fields = ['username', 'first_name', 'last_name', 'email', 'password1', 'password2']
